69c41652c54a5b9f49a10a1b6065e5369ec63fa3
JOSEPH-8546-cyber <manueljosephtrabajo@gmail.com>
2021-01-27 22:14:21 +0000

    se cambio car por motorcycle

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let who = ['The dog','My grandma','His turtle','My bird']
 
 let action = ['ate','peed','crushed','broke'];
 
-let what = ['my homework', 'the keys', 'the car'];
+let what = ['my homework', 'the keys', 'the motocircle'];
 
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 

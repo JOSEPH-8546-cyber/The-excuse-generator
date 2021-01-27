42c3b58a4052040f3e0b9a4de67ce8ef94d3c3a2
JOSEPH-8546-cyber <manueljosephtrabajo@gmail.com>
2021-01-27 21:51:04 +0000

    trabajo terminado

---
+++ index.js
@@ -0,0 +1,23 @@
+function randomExcuse(){
+
+let who = ['The dog','My grandma','His turtle','My bird']
+
+let action = ['ate','peed','crushed','broke'];
+
+let what = ['my homework', 'the keys', 'the car'];
+
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+
+let randomnumber1= Math.floor(Math.random() * 4)
+let randomnumber2= Math.floor(Math.random() * 3)
+let randomnumber3= Math.floor(Math.random() * 3)
+let randomnumber4= Math.floor(Math.random() * 5)
+
+let text = `${who[randomnumber1]} ${when[randomnumber2]} ${what[randomnumber3]} ${action[randomnumber4]}`
+let parrafo= document.querySelector("#excuse")
+parrafo.innerHTML= text
+};
+
+randomExcuse()
+
